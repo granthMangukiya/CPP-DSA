94f14434345fbe50878f4264ecf567d14a160d94
Granth Mangukiya <granth0407@gmail.com>
2025-04-08 18:49:47 +0530

    If Else completed

---
+++ IfElse.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a = -47;
+    
+
+if (a<0)
+{
+    cout << "The number is Negative.";
+}
+else
+{
+    cout << "The number is Positive";
+}
+
+    
+    
+
+    return 0;
+}
+
+
+
