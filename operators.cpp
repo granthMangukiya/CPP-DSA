1995cb6b56841467a8063340b4f8850bad64a530
Granth Mangukiya <granth0407@gmail.com>
2025-04-08 18:21:28 +0530

    Operators completed

---
+++ operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 11, b = 5;
+
+    cout << "sum = " << a + b << "\n";
+    cout << "Difference = " << a - b << "\n";
+    cout << "product = " << a * b << "\n";
+    cout << "division = " << a / b << "\n";
+    cout << "modulo = " << a % b << "\n";
+    return 0;
+
+}
