9229df3f10ad1cf8aa8daa8525f40b7203e010ff
Granth Mangukiya <granth0407@gmail.com>
2025-04-10 12:13:07 +0530

    Do While Loop Completed

---
+++ DoWhileLoop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n=10 ;
+    int i = 1;
+
+    do
+    {
+        cout << i << "\n";
+        i++;
+    } while (i<=n);
+    
+
+    return 0;
+}
