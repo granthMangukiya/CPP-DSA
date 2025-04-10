8cf59d371e84831618794823248796dfe1cece3d
Granth Mangukiya <granth0407@gmail.com>
2025-04-10 11:29:48 +0530

    While Loop Completed

---
+++ WhileLoop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int count = 1;
+
+    while (count <= 5)
+    {
+        cout << count << "\n";
+        count ++;
+    }
+    
+
+    return 0;
+}
