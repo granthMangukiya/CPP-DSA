21bb275dc29605ef0a2197ab5b5b41d2c1b9693a
Granth Mangukiya <granth0407@gmail.com>
2025-04-13 11:26:19 +0530

    Square Pattern Completed

---
+++ SquarePattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n = 5;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int x = 1 ; x <= n ; x++)
+        {
+            cout << x;
+        }
+        cout << endl;
+    }
+    
+
+    return 0;
+}
