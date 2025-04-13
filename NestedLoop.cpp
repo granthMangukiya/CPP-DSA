9b326351cf3448ccd82966ccb0cf12da1bd32c08
Granth Mangukiya <granth0407@gmail.com>
2025-04-13 11:11:27 +0530

    Nested Loops completed

---
+++ NestedLoop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m = 10;
+    for (int i = 1; i <=5; i++)
+    {
+        for (int i = 1; i <= m; i++)
+        {
+            cout << "x";
+        }
+
+        cout << endl;
+        
+    }
+    
+
+    return 0;
+}
