ab133f51d4f6bc665941c15c6e521233b0c199ea
Granth Mangukiya <granth0407@gmail.com>
2025-06-07 10:13:53 +0530

    Edit

--- BinomialCoefficient.cpp
+++ BinomialCoefficient.cpp
@@ -17,11 +17,9 @@ int nCr(int n, int r)
 
     int fact_n = factorial(n);
     int fact_r = factorial(r);
-    int fact_nmr = factorial(n-r);
+    int fact_nmr = factorial(n - r);
 
     return fact_n / (fact_r * fact_nmr);
-
-    
 }
 
 int main()
@@ -29,6 +27,6 @@ int main()
     int n = 8;
     int r = 2;
 
-    cout << nCr(n,r) << endl;
+    cout << nCr(n, r) << endl;
     return 0;
 }
